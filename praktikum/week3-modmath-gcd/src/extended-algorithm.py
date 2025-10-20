1a8f56a7332a5e4c9655ff6d2a717fa8b9d163f3
ellmhr <laelimaharani09@gmail.com>
2025-10-20 13:21:48 +0700

    Add files via upload

---
+++ praktikum/week3-modmath-gcd/src/extended-algorithm.py
@@ -0,0 +1,13 @@
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    g, x1, y1 = egcd(b % a, a)
+    return g, y1 - (b // a) * x1, x1
+
+def modinv(a, n):
+    g, x, _ = egcd(a, n)
+    if g != 1:
+        return None
+    return x % n
+
+print("Invers 3 mod 11 =", modinv(3, 11))  # hasil: 4
